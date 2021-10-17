05f36931f054ce8eb586d39428f177dbab8390e4
HCtoutou <81293636+HCtoutou@users.noreply.github.com>
2021-10-17 12:09:44 +0800

    Create HashSep.h

---
+++ week3/ch01/HashSep.h
@@ -0,0 +1,25 @@
+#pragma once
+struct ListNode;
+typedef struct ListNode* Position;
+struct HashTbl;
+typedef struct HashTbl* HashTable;
+
+HashTable InitializeTable(int TableSize);
+int Hash(int Key, int TableSize);
+void DestroyTable(HashTable H);
+Position Find(int Key, HashTable H);
+Position FindPre(int Key, HashTable H);//返回Key值前面的节点，删除时需要
+void Insert(int Key,HashTable H);
+HashTable Delete(int Key, HashTable H);
+
+struct ListNode {
+	int Element;
+	Position Next;
+};
+typedef Position List;
+
+
+struct HashTbl {
+	int TableSize;
+	List* TheLists;
+};
