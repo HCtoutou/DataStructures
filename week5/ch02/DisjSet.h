dd5856b4c33b8853971846ac1207fe5605deaaad
HCtoutou <81293636+HCtoutou@users.noreply.github.com>
2022-01-27 14:51:13 +0800

    Delete week5 directory

--- week5/ch02/DisjSet.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#define NumSets 10
-typedef int DisjSet[NumSets + 1];
-typedef int SetType;
-typedef int ElemenType;
-
-void Initialize(DisjSet S);
-void SetUnion(DisjSet S, SetType Root1, SetType Root2);
-SetType Find(ElemenType X, DisjSet S);
